d91b7577a0614922580ab7dfbc68828289818aac
matthew-heideman <heidemanmr@yahoo.com>
2020-02-28 11:36:29 -0600

    completed arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -45,12 +45,11 @@ console.log(dino3.diet);
 console.log(dino2.length);
 
 // What time period did tyrannosaurus live in?
-console.log(dino.period);
+console.log(dino1.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 console.log(dino1.roar());
-
 
 // ==== Arrays ====
 
@@ -73,6 +72,10 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for(i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university);
+  universities.sort();
+}
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -82,6 +85,9 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+for (i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
